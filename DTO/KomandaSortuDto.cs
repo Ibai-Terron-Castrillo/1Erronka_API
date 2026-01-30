7c71298f9c3c9be28df85edfe59e318a1720ef8f
Ane <ane_sot_azp@goierrieskola.org>
2026-01-30 14:50:37 +0100

    Azken aldaketak(Espero)

--- DTO/KomandaSortuDto.cs
+++ DTO/KomandaSortuDto.cs
@@ -5,3 +5,26 @@ public class KomandaSortuDto
     public int Kopurua { get; set; }
     public double Totala { get; set; }
 }
+
+public class KomandaDto
+{
+    public int Id { get; set; }
+    public int FakturakId { get; set; }
+    public int PlaterakId { get; set; }
+    public int Kopurua { get; set; }
+    public double Totala { get; set; }
+    public string Oharrak { get; set; }
+    public bool Egoera { get; set; }
+    public PlaterakRefDto Platerak { get; set; }
+    public FakturaRefDto Faktura { get; set; }
+}
+
+public class PlaterakRefDto
+{
+    public int Id { get; set; }
+}
+
+public class FakturaRefDto
+{
+    public int Id { get; set; }
+}
